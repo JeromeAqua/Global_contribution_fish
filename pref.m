fa0647c354f64a1be32b29da6c96b16aa52e2fae
Jerome Pierre Alexandre Pinti <jppi@aqua.dtu.dk>
2019-07-11 09:51:24 +0200

    Created preference function

---
+++ pref.m
@@ -0,0 +1,75 @@
+function out = pref(predator,prey)
+% [-] Preference values for each prey and predator
+    
+if strcmp(predator,'copepod')
+    if strcmp(prey,'phyto')
+        out = 1;
+    elseif strcmp(prey,'detritus')
+        out = 0.5;
+    else
+        out = NaN;
+    end   
+    
+elseif strcmp(predator,'forage')
+    if strcmp(prey,'detritus')
+        out = 0.2;
+    elseif strcmp(prey,'benthos')
+        out = 0.2;
+    elseif strcmp(prey,'copepod')
+        out = 1;
+    elseif strcmp(prey,'meso')
+        out = 0.5;
+    else
+        out = NaN;
+    end  
+    
+elseif strcmp(predator,'top')
+    if strcmp(prey,'forage')
+        out = 1;
+    elseif strcmp(prey,'tactile')
+        out = 0.2;
+    elseif strcmp(prey,'meso')
+        out = 1;
+    elseif strcmp(prey,'bathy')
+        out = 1;
+    else
+        out = NaN;
+    end        
+              
+elseif strcmp(predator,'tactile')
+    if strcmp(prey,'copepod')
+        out = 1;
+    elseif strcmp(prey,'meso')
+        out = 1;
+    else
+        out = NaN;
+    end                
+             
+elseif strcmp(predator,'meso')
+    if strcmp(prey,'detritus')
+        out = 0.8;
+    elseif strcmp(prey,'copepod')
+        out = 1;
+    else
+        out = NaN;
+    end                             
+                
+elseif strcmp(predator,'bathy')
+    if strcmp(prey,'detritus')
+        out = 0.8;
+    elseif strcmp(prey,'benthos')
+        out = 1;
+    elseif strcmp(prey,'copepod')
+        out = 0.5;
+    elseif strcmp(prey,'meso')
+        out = 1;
+    else
+        out = NaN;
+    end                 
+                                        
+end
+
+end
+
+
+
