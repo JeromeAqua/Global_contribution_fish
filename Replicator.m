1536d926c01ce955a18f557454972cd85f725797
Jerome Pierre Alexandre Pinti <jppi@aqua.dtu.dk>
2019-07-15 14:01:42 +0200

    Started to add the mortality rates

--- Replicator.m
+++ Replicator.m
@@ -66,13 +66,13 @@ NB0 = P.INB + P.ENB.*(pref('bathy','detritus')*repmat(P.D,P.n,1)+pref('bathy','b
                       pref('bathy','copepod')*repmat(Cnight,P.n,1)+pref('bathy','meso')*repmat(Mnight,P.n,1)); % [gC day^-1] Denominator for the ingestion function  
 
 
-%Ingestion functions
+%Ingestion rates
 IFC1 = P.IDF.*P.EDF*pref('forage','copepod').* repmat(Cday' ,1,P.n)./NF1; % [gC day^-1] Ingestion rate of copepods during daytime by forage fish
 IFC0 = P.INF.*P.ENF*pref('forage','copepod').* repmat(Cnight,P.n,1)./NF0;
 IFD1 = P.IDF.*P.EDF*pref('forage','detritus').*repmat(P.D',1,P.n)  ./NF1;
 IFD0 = P.INF.*P.ENF*pref('forage','detritus').*repmat(P.D ,P.n,1)  ./NF0;
 IFb1 = P.IDF.*P.EDF*pref('forage','benthos') .*repmat(P.Benthos',1,P.n)./NF1; % [gC day^-1] small b to show that it is for benthos and not bathypelagic fish
-IFB0 = P.INF.*P.ENF*pref('forage','benthos') .*repmat(P.Benthos, P.n,1)./NF0;
+IFb0 = P.INF.*P.ENF*pref('forage','benthos') .*repmat(P.Benthos, P.n,1)./NF0;
 IFM1 = P.IDF.*P.EDF*pref('forage','meso')    .*repmat(Mday' ,1,P.n)./NF1;
 IFM0 = P.INF.*P.ENF*pref('forage','meso')    .*repmat(Mnight,P.n,1)./NF0;
 
@@ -108,3 +108,56 @@ IBC1 = P.IDB.*P.EDB*pref('bathy','copepod') .*repmat(Cday',1,P.n)./NB1; % [gC da
 IBC0 = P.INB.*P.ENB*pref('bathy','copepod') .*repmat(Cnight, P.n,1)./NB0;
 IBM1 = P.IDB.*P.EDB*pref('bathy','meso')    .*repmat(Mday' ,1,P.n)./NB1;
 IBM0 = P.INB.*P.ENB*pref('bathy','meso')    .*repmat(Mnight,P.n,1)./NB0;
+
+%Assimilation rates
+IC = P.fC*(P.sigma*(ICR1+ICD1)+(1-P.sigma)*(ICR0+ICD0))/P.wC; % [day^-1] Total assimilation rate per individual per strategy for copepods
+IF = P.fF*(P.sigma*(IFD1+IFb1+IFC1+IFM1)+(1-P.sigma)*(IFD0+IFb0+IFC0+IFM0))/P.wF; % [day^-1] Total assimilation rate per individual per strategy for forage fish
+IA = P.fA*(P.sigma*(IAF1+IAJ1+IAM1+IAB1)+(1-P.sigma)*(IAF0+IAJ0+IAM0+IAB0))/P.wA; % [day^-1] Total assimilation rate per individual per strategy for top predator
+IJ = P.fJ*(P.sigma*(IJC1+IJM1)+(1-P.sigma)*(IJC0+IJM0))/P.wJ; % [day^-1] Total assimilation rate per individual per strategy for tactile predator
+IM = P.fM*(P.sigma*(IMD1+IMC1)+(1-P.sigma)*(IMD0+IMC0))/P.wM; % [day^-1] Total assimilation rate per individual per strategy for mesopelagic fish
+IB = P.fB*(P.sigma*(IBD1+IBb1+IBC1+IBM1)+(1-P.sigma)*(IBD0+IBb0+IBC0+IBM0))/P.wB; % [day^-1] Total assimilation rate per individual per strategy for bathypelagic fish
+
+%Mortality rates - we calculate the mortality rate imposed by all strategies (predators) at each depth before redistributing it equally among prey
+%Forage fish
+mFday = IAF1*P.n^2*P.A.*A/P.wA; % [gC m^-3 day^-1] size n*n How much each strategy eats forage fish during daytime
+mFD = (sum(mFday,2)./Fday')'; % [day^-1] size 1*n What is the mortality rate experienced at each depth during day
+MortD = F.*repmat(mFD',1,P.n); % [day^-1] Mortality rate experienced by the different forage fish strategies during daytime
+
+mFnight = IAF0*P.n^2*P.A*A/P.wA; % [gC m^-3 day^-1]
+mFN = sum(mFnight,1)./Fnight; % [day^-1]
+MortN = F.*repmat(mFN,P.n,1); % [day^-1] Mortality rate experienced by the different forage fish strategies during nighttime
+
+MortF = P.sigma*MortD + (1-P.sigma)*MortN; % [day^-1] Total mortality rate experienced by the different forage fish strategies
+
+%Forage fish
+mFday = IAF1*P.n^2*P.A.*A/P.wA; % [gC m^-3 day^-1] size n*n How much each strategy eats forage fish during daytime
+mFD = (sum(mFday,2)./Fday')'; % [day^-1] size 1*n What is the mortality rate experienced at each depth during day
+MortD = F.*repmat(mFD',1,P.n); % [day^-1] Mortality rate experienced by the different forage fish strategies during daytime
+
+mFnight = IAF0*P.n^2*P.A*A/P.wA; % [gC m^-3 day^-1]
+mFN = sum(mFnight,1)./Fnight; % [day^-1]
+MortN = F.*repmat(mFN,P.n,1); % [day^-1] Mortality rate experienced by the different forage fish strategies during nighttime
+
+MortF = P.sigma*MortD + (1-P.sigma)*MortN; % [day^-1] Total mortality rate experienced by the different forage fish strategies
+
+%Tactile predator
+mJday = IAJ1*P.n^2*P.A.*A/P.wA; % [gC m^-3 day^-1] size n*n How much each strategy eats tactile pred during daytime
+mJD = (sum(mJday,2)./Jday')'; % [day^-1] size 1*n What is the mortality rate experienced at each depth during day
+MortD = J.*repmat(mJD',1,P.n); % [day^-1] Mortality rate experienced by the different tactile pred strategies during daytime
+
+mJnight = IAJ0*P.n^2*P.A*A/P.wA; % [gC m^-3 day^-1]
+mJN = sum(mJnight,1)./Jnight; % [day^-1]
+MortN = J.*repmat(mJN,P.n,1); % [day^-1] Mortality rate experienced by the different tactile pred strategies during nighttime
+
+MortJ = P.sigma*MortD + (1-P.sigma)*MortN; % [day^-1] Total mortality rate experienced by the different tactile predator strategies
+
+%Bathypelagic fish
+mBday = IAB1*P.n^2*P.A.*A/P.wA; % [gC m^-3 day^-1] size n*n How much each strategy eats bathypelagic fish during daytime
+mBD = (sum(mBday,2)./Bday')'; % [day^-1] size 1*n What is the mortality rate experienced at each depth during day
+MortD = B.*repmat(mBD',1,P.n); % [day^-1] Mortality rate experienced by the different bathypelagic fish strategies during daytime
+
+mBnight = IAB0*P.n^2*P.A*A/P.wA; % [gC m^-3 day^-1]
+mBN = sum(mBnight,1)./Bnight; % [day^-1]
+MortN = B.*repmat(mBN,P.n,1); % [day^-1] Mortality rate experienced by the different bathypelagic fish strategies during nighttime
+
+MortB = P.sigma*MortD + (1-P.sigma)*MortN; % [day^-1] Total mortality rate experienced by the different bathypelagic fish strategies
